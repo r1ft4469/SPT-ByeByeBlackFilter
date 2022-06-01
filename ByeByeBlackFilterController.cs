aeb47636fc2ef66c75b9e1e9e79429e2eb6fe784
Penn Oser <r1ft4469@users.noreply.github.com>
2022-06-01 16:53:01 -0500

    Inital Commit

---
+++ ByeByeBlackFilterController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+namespace r1ft.ByeByeBlackFilter
+{
+    public class ByeByeBlackFilterController : MonoBehaviour
+    {
+        private bool Enabled { get; set; } = false;
+        private ColorCorrectionCurves CurvesCache { get; set; } = null;
+
+        public void Update()
+        {
+            if (Camera.main == null)
+            {
+                CurvesCache = null;
+                Enabled = false;
+                return;
+            }
+
+            if (CurvesCache == null)
+                Set();
+
+            return;
+        }
+
+        private void Set()
+        {
+            if (CurvesCache == null)
+            {
+                CurvesCache = Camera.main.GetComponent<ColorCorrectionCurves>();
+                if (CurvesCache == null)
+                    return;
+            }
+
+            CurvesCache.enabled = Enabled;
+            Enabled = !Enabled;
+        }
+    }
+}
---
+++ ByeByeBlackFilterPlugin.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using BepInEx;
+using BepInEx.Configuration;
+
+namespace r1ft.ByeByeBlackFilter
+{
+    [BepInPlugin("com.r1ft.ByeByeBlackFilter", "r1ft.ByeByeBlackFilter", "1.0.0")]
+    public class ByeByeBlackFilter : BaseUnityPlugin
+    {
+        public static GameObject Hook;
+
+        private void Awake()
+        {
+            Logger.LogInfo("Loading: r1fT-ByeByeBlackFilter");
+            Hook = new GameObject("ByeByeBlackFilter");
+            Hook.AddComponent<ByeByeBlackFilterController>();
+            DontDestroyOnLoad(Hook);
+        }
+    }
+}
